8af2dde49032168d4f000c53a050dd30361e999a
Samuel Joli <samuel.joli.ftn@gmail.com>
2023-01-17 01:49:49 -0500

    Install LSP-Zero, workspaces and projectconfig

--- private_dot_config/nvim/lua/user/plugins/init.lua
+++ private_dot_config/nvim/lua/user/plugins/init.lua
@@ -8,6 +8,12 @@ require('user.plugins.bufferline.setup')
 
 require('user.plugins.bbye.setup')
 
+require('user.plugins.workspaces.setup')
+
+require('user.plugins.projectconfig.setup')
+
+require('user.plugins.lsp_zero.setup')
+
 return require('user.plugins.packer.setup').startup(function(use)
 
   use 'wbthomason/packer.nvim'       -- Have packer manage itself
@@ -28,6 +34,29 @@ return require('user.plugins.packer.setup').startup(function(use)
       requires = 'nvim-tree/nvim-web-devicons'
   }
   use 'moll/vim-bbye'                -- better buffer delete
+  use {
+      'VonHeikemen/lsp-zero.nvim',
+      requires = {
+        -- LSP Support
+        {'neovim/nvim-lspconfig'},             -- Required
+        {'williamboman/mason.nvim'},           -- Optional
+        {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+        -- Autocompletion
+        {'hrsh7th/nvim-cmp'},         -- Required
+        {'hrsh7th/cmp-nvim-lsp'},     -- Required
+        {'hrsh7th/cmp-buffer'},       -- Optional
+        {'hrsh7th/cmp-path'},         -- Optional
+        {'saadparwaiz1/cmp_luasnip'}, -- Optional
+        {'hrsh7th/cmp-nvim-lua'},     -- Optional
+
+        -- Snippets
+        {'L3MON4D3/LuaSnip'},             -- Required
+        {'rafamadriz/friendly-snippets'}, -- Optional
+      }
+  }
+  use 'natecraddock/workspaces.nvim'   --  workspaces
+  use 'windwp/nvim-projectconfig'      -- project specific config files
 
   if PACKER_BOOTSTRAP then           -- Automatically set up your configuration after cloning packer.nvim
     require("packer").sync()
