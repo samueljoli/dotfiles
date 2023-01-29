e64c31ead37b6e9f1b0bb0279627548eb83b44d8
Samuel Joli <samuel.joli.ftn@gmail.com>
2023-01-29 11:41:50 -0500

    Add footer to nvim dashboard

--- private_dot_config/nvim/lua/user/plugins/alpha/setup.lua
+++ private_dot_config/nvim/lua/user/plugins/alpha/setup.lua
@@ -3,6 +3,14 @@ if not status_ok then
   return
 end
 
+local function footer()
+  local plugins_count = vim.fn.len(vim.fn.globpath("~/.local/share/nvim/site/pack/packer/start", "*", 0, 1))
+  local datetime = os.date("  %m-%d-%Y   %H:%M:%S")
+  local version = vim.version()
+  local nvim_version_info = "   v" .. version.major .. "." .. version.minor .. "." .. version.patch
+  return datetime .. "   Plugins " .. plugins_count .. nvim_version_info
+end
+
 local dashboard = require("alpha.themes.dashboard")
 dashboard.section.header.val = {
   [[                                                 ]],
@@ -47,8 +55,7 @@ dashboard.section.buttons.val = {
 --   return "chrisatmachine.com"
 -- end
 
--- dashboard.section.footer.val = footer()
-
+dashboard.section.footer.val = footer()
 dashboard.section.footer.opts.hl = "Type"
 dashboard.section.header.opts.hl = "Include"
 dashboard.section.buttons.opts.hl = "Keyword"
